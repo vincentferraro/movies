1b910df0f7571f94002797f106cebff0f8d83c44
Vincent Ferraro <v.ferraro@live.fr>
2022-08-09 11:13:00 +0200

    first update

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movies/{name}', name: 'movies')]
+    public function index(): Response
+    {
+        $movies=['Avengers:Endgame','Inception','Loki','Black Widow'];
+
+        return $this->render('index.html.twig',[
+            'movies'=>$movies
+        ]);
+    }
+}
