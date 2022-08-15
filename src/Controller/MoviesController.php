46f07c62f8cca28aae7f383e566b80737849e35a
Vincent Ferraro <v.ferraro@live.fr>
2022-08-15 16:22:09 +0200

    add EntityManagerInterface

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MoviesController extends AbstractController
-{
-    #[Route('/movies/{name}', name: 'movies')]
-    public function index(): Response
+    class MoviesController extends AbstractController
     {
-        $movies=['Avengers:Endgame','Inception','Loki','Black Widow'];
 
-        return $this->render('index.html.twig',[
-            'movies'=>$movies
-        ]);
+        private $em;
+        public function __construct(EntityManagerInterface $em)
+        {
+            $this->em=$em;
+            
+        }
+        #[Route('/movies', name: 'movies')]
+        public function index(): Response
+        {
+            $repository= $this->em -> getRepository(Movie::class);
+            $movies = $repository->findAll();
+            dd($movies);
+
+            return $this->render('index.html.twig');
+        }
+
+        
     }
-}
