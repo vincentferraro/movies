4e576eba9c1c82ccb1bbd9328e50526d3efe638d
Vincent Ferraro <v.ferraro@live.fr>
2022-08-16 16:33:18 +0200

    add front

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,27 +2,29 @@
 
 namespace App\Controller;
 
-use App\Entity\Movie;
 use App\Repository\MovieRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
     class MoviesController extends AbstractController
     {
+        private $movieRepository;
 
-        private $em;
-        public function __construct(EntityManagerInterface $em)
+        public function __construct(MovieRepository $movieRepository)
         {
-            $this->em=$em;
+            $this->movieRepository = $movieRepository;
             
         }
+
         #[Route('/movies', name: 'movies')]
         public function index(): Response
         {
-                
-            return $this->render('index.html.twig');
+            $movies = $this->movieRepository->findAll();
+            
+            return $this->render('movies/index.html.twig',[
+                'movies'=> $movies
+            ]);
         }
 
         
